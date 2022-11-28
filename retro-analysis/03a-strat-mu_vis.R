776b65dc3b5a7ae99d2b5a2528c1c8b6c65418e0
gavinfay <gfay42@gmail.com>
2022-11-28 14:46:11 -0500

    adds species lookup and doesn't print plots to screen

--- retro-analysis/03a-strat-mu_vis.R
+++ retro-analysis/03a-strat-mu_vis.R
@@ -33,7 +33,7 @@ mu_plots <- list()
 
 #### FORMAT GGPLOT FOR LOOP ####
 
-x <- stratmeans %>% filter(SVSPP == 141)
+x <- data %>% filter(SVSPP == 141)
 
 y <- ggplot(x) + 
   geom_line(aes(EST_YEAR, stratmu, color = TYPE)) + 
@@ -52,11 +52,20 @@ for(i in species){ # for each value i in the species vector
     geom_line(aes(EST_YEAR, # x value 
                   stratmu, # y value - stratified mean value
                   color = TYPE)) + # plot all values and differentiate based on whether or not wind tows were included in the calculation 
-    labs(x = "YEAR", y = "Stratified Mean (kg/tow)", title = x$COMNAME) # edit plot labels 
+    labs(x = "YEAR", y = "Stratified Mean (kg/tow)", title = x$COMNAME,
+         TYPE = "") + # edit plot labels 
+    theme(legend.position="bottom",
+          legend.title = element_blank())
 }
 
 ### save the plot list 
 saveRDS(mu_plots, file = here("data", "rds", "mu-species-plots.rds"))
+
+# create a lookup table with filename-friendly species names
+specieslookup <- data %>% 
+  select(SVSPP, COMNAME) %>% 
+  distinct() %>% 
+  mutate(spname = str_to_lower(gsub(" ", "_", COMNAME)))
 
 
 # Loop 02 - loop over the species vector, print and save the plot within the list object
@@ -64,9 +73,10 @@ for(i in seq_along(species)){ # move along the sequence of values in the species
   
   if(is.null(mu_plots[[i]])) next # if the sequential value i in the plot list is NULL, skip the iteration and move on to the next step in the sequence
   
-   print(mu_plots[[i]]) # print the plot value corresponding with value i
+#   print(mu_plots[[i]]) # print the plot value corresponding with value i
   
   # save the printed plot with the species name based on the list value generated
-  ggsave(filename = paste(unique(mu_plots[[i]]$data$COMNAME), ".png"), device = "png", path = here("outputs", "plots", "strat_mu"), width = 5, height = 5)
+  #ggsave(filename = paste(unique(mu_plots[[i]]$data$COMNAME), ".png"), device = "png", path = here("outputs", "plots", "strat_mu"), width = 5, height = 5)
+  ggsave(filename = paste0(specieslookup$spname[i], ".png"), device = "png" , plot = mu_plots[[i]], path = here("outputs", "plots", "strat_mu"), width = 5, height = 5)
 
 }
