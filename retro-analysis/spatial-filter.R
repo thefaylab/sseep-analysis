c0c937d473c7ae5c9d1ba2b81e7f8999ad0098c5
Lia Miller <amiller7@umassd.edu>
2023-02-06 13:58:42 -0500

    apply strata selection to full dataset based on 95% and 99% biomass observations

--- retro-analysis/spatial-filter.R
+++ retro-analysis/spatial-filter.R
@@ -1,12 +1,12 @@
-### created: 1/27/2022
-### last updated: 
+### created: 1/27/2023
+### last updated: 2/6/2023 
 
-#### 00 - SPATIAL FILTERING ####
+#### 01 - SPATIAL FILTERING ####
 
 ###################
 #### OBJECTIVE ####
 ###################
-# 
+# find the strata that make up 95% of the cumulative distribution of biomass for each species and season, and use it to filter the dataset so that the stratified means are only calculated with the strata that have 95% of the cumulative biomass. 
 
 ####################
 
@@ -24,40 +24,58 @@ suppressPackageStartupMessages(library(tidyverse))
 data <- readRDS(here("data", "rds", "merged_data_complete.rds")) %>% mutate(EXPCATCHWT = ifelse(is.na(EXPCATCHWT), 0, EXPCATCHWT))
 
 
-#### FILTER DATA BASED ON STRATA ####
+#### FIND TOTAL BIOMASS ####
 #find the total biomass for each species and season combination by strata 
 total_bio <- data %>% 
-  group_by(COMNAME, SEASON, STRATUM) %>% 
+  group_by(SVSPP, COMNAME, SEASON, STRATUM) %>% 
   summarise(EXPCATCHWT = sum(EXPCATCHWT), .groups = "drop") %>%  # sum biomass by groups, and drop groups once calculation is complete 
-  group_by(COMNAME, SEASON) %>% #regroup by common name and season
-  arrange(desc(EXPCATCHWT), desc(STRATUM)) #arrange dataframe in descending order based on total biomass and stratum.
+  group_by(SVSPP, COMNAME, SEASON) %>% #regroup by common name and season
+  arrange(desc(EXPCATCHWT)) #arrange dataframe in descending order based on total biomass and stratum.
 
 ### save data 
 saveRDS(total_bio, here("data", "rds", "total_biomass.rds"))
 
 
-# FIND CUMULATIVE DISTRIBUTION 
-# test data
-summerflounder_fall <- total_bio %>% 
-  filter(COMNAME == "SUMMER FLOUNDER", SEASON == "FALL") %>% 
-  mutate(csum_bio = cumsum(EXPCATCHWT),
-         prop = csum_bio/max(csum_bio))
-
-
-
-
-
-# cumulative distribution function 
-# cd <- ecdf(summerflounder$EXPCATCHWT)
+#### FIND CUMULATIVE DISTRIBUTION ####
 # 
-# plot(cd, verticals = TRUE, col.points = "blue", col.hor = "red", col.vert = "bisque")
-# 
-# # call function against biomass; returns the percentiles for biomass
-# x <- cd(summerflounder$EXPCATCHWT)
-# 
-# # attach to test dataframe 
-# summerflounder <- bind_cols(summerflounder, x)
 
+#### Test Data ####
+# summerflounder_fall <- total_bio %>% 
+#   filter(COMNAME == "SUMMER FLOUNDER", SEASON == "FALL") %>% 
+#   mutate(csum_bio = cumsum(EXPCATCHWT),
+#          prop = csum_bio/max(csum_bio))
 
-
+#####
+# filter 95% of the distribution 
+csum_dist95 <- total_bio %>% 
+  group_by(SVSPP, COMNAME, SEASON) %>% 
+  mutate(csum_bio = cumsum(EXPCATCHWT), # calculate the cumulative sum of biomass
+         prop = round(csum_bio/max(csum_bio), 2)) %>% # divide each value of cumulative sum by the maximum sum for each COMNAME and SEASON combination to find the proportion each sum makes up of the total distribution of sums
+  filter(prop <= 0.95) # filter the data where the proportion is less than or equal to 95% of the total distribution of biomass for each COMNAME and SEASON group to identify which strata make up the majority of the biomass.
   
+
+### save data 
+#saveRDS(csum_dist95, here("data", "rds", "cumul-dist-biomass95.rds"))
+
+# filter 99% of the distribution
+csum_dist99 <- total_bio %>% 
+  group_by(SVSPP, COMNAME, SEASON) %>% 
+  mutate(csum_bio = cumsum(EXPCATCHWT), # calculate the cumulative sum of biomass
+         prop = round(csum_bio/max(csum_bio), 2)) %>% # divide each value of cumulative sum by the maximum sum for each COMNAME and SEASON combination to find the proportion each sum makes up of the total distribution of sums
+  filter(prop <= 0.99) # filter the data where the proportion is less than or equal to 99% of the total distribution of biomass for each COMNAME and SEASON group to identify which strata make up the majority of the biomass.
+
+### save data 
+#saveRDS(csum_dist99, here("data", "rds", "cumul-dist-biomass99.rds"))
+
+
+#### FILTER FULL DATASET ####
+# filter the full dataset based on the same observations that appear in the cumulative distribution dataset
+data95 <- semi_join(data, csum_dist95, by = c("SVSPP", "SEASON", "STRATUM"))
+
+# filters from 3962124 to 287533 -- 7% of the full data set? 
+
+data99 <- semi_join(data, csum_dist99, by = c("SVSPP", "SEASON", "STRATUM"))
+# filters from 3962124 to 287533 -- 10% of the full data set? 
+
+### save data 
+#saveRDS(data, here("data", "rds", "filtered_complete_bts.rds"))
