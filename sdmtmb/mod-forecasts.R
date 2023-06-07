4c9bbb7c29f2f41a44b8d7f0a977e13548a01b61
Lia Miller <amiller7@umassd.edu>
2023-06-07 08:13:30 -0400

    create two seasonal grids based on historical distribution of summer flounder from the full survey area grid

--- sdmtmb/mod-forecasts.R
+++ sdmtmb/mod-forecasts.R
@@ -43,15 +43,61 @@ sf_spring <- readRDS(here(sdmtmb.dir, "data", "sumflounder_spring.rds"))
 # read in the spring mesh for model fitting  
 spring_mesh <- readRDS(here(sdmtmb.dir, "data", "spring_mesh.rds"))
 
-### THE GRID ####
-# read in the grid with the area predictor created here(sdmtmb.dir, "grids")
-grid <- readRDS(here(sdmtmb.dir, "data", "survey_grid.rds")) |> 
-  mutate(X = X/1000, 
-         Y = Y/1000)
-         
 # define extra years to forecast 
 fall_extra_years <- c(2020, 2022:2026)
 spring_extra_years <- c(2022:2026)
+
+### THE GRID ####
+# read in the grid with the area predictor created here(sdmtmb.dir, "grids")
+grid <- readRDS(here(sdmtmb.dir, "data", "survey_grid.rds")) 
+
+## DATA WRANGLE ####
+grid_sf <- grid |> 
+  st_as_sf(coords = c("X", "Y"))
+st_crs(grid_sf) <-32618
+
+### FALL GRID ####
+sf_fall_strat <- right_join(strata, sf_fall, by = "STRATUM")
+fall_strat_utm <- st_transform(sf_fall_strat, crs = 32618)
+fall_strat_union <- st_union(fall_strat_utm)
+intersected <- sf::st_intersects(grid_sf, fall_strat_union)
+selected_grid <- grid_sf[lengths(intersected) > 0, ]  
+
+coords <- selected_grid |>
+  st_coordinates()
+  
+fall_grid <- bind_cols(coords, selected_grid) |> 
+  select(-geometry) |>
+  mutate(X = X/1000,
+         Y = Y/1000, 
+         SEASON = "FALL")
+
+ggplot(fall_grid, aes(X, Y, fill = AVGDEPTH)) + 
+  geom_tile(width = 10, height = 10) +
+  scale_fill_viridis_c() +
+  coord_equal()
+
+### SPRING GRID ####
+sf_spr_strat <- right_join(strata, sf_spring, by = "STRATUM")
+spr_strat_utm <- st_transform(sf_spr_strat, crs = 32618)
+spr_strat_union <- st_union(spr_strat_utm)
+intersected <- sf::st_intersects(grid_sf, spr_strat_union)
+selected_grid <- grid_sf[lengths(intersected) > 0, ]  
+
+coords <- selected_grid |>
+  st_coordinates()
+
+spr_grid <- bind_cols(coords, selected_grid) |> 
+  select(-geometry) |>
+  mutate(X = X/1000,
+         Y = Y/1000, 
+         SEASON = "SPRING")
+
+ggplot(spr_grid, aes(X, Y, fill = AVGDEPTH)) + 
+  geom_tile(width = 10, height = 10) +
+  scale_fill_viridis_c() +
+  coord_equal()
+
 
 #### MODEL FITS ####
 # re run model 11 from here(sdmtmb.dir, "02a-fit-fall-mods.R") with forecasted years, and REML off 
@@ -88,11 +134,12 @@ saveRDS(spring_mod, here("sdmtmb", "mar-536-project", "data", "spring_mod.rds"))
 
 #### MAKE PREDICTIONS ####
 # replicate our grid across all necessary years
-grid <- sdmTMB::replicate_df(grid, "EST_YEAR", c(2009:2026)) 
+fall_grid <- sdmTMB::replicate_df(fall_grid, "EST_YEAR", c(2009:2026)) 
+spr_grid <- sdmTMB::replicate_df(spr_grid, "EST_YEAR", c(2009:2026)) 
 
 # predictions
-fall_preds <- predict(fall_mod, newdata = grid)#, return_tmb_object = TRUE)# |> as.data.frame()
-spring_preds <- predict(spring_mod, newdata = grid)#, return_tmb_object = TRUE) #|> as.data.frame()
+fall_preds <- predict(fall_mod, newdata = fall_grid)#, return_tmb_object = TRUE)# |> as.data.frame()
+spring_preds <- predict(spring_mod, newdata = spr_grid)#, return_tmb_object = TRUE) #|> as.data.frame()
 
 ### save the predictions
 saveRDS(fall_preds, file = here(sdmtmb.dir, "data", "fall_projects.rds"))
