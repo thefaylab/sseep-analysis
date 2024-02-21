3e7e901a198d12b5d05f77594ad1e3b4e9bc2460
Lia Miller <amiller7@umassd.edu>
2024-02-21 11:24:48 -0500

    add prep script for simulations

---
+++ sdmtmb/R/prep-sim-data.R
@@ -0,0 +1,181 @@
+### created: 02/21/2024
+### updated: 
+
+# PREPARE DATA FOR SIMULATIONS  ####
+
+## OBJECTIVE ####
+
+
+### LOAD PACKAGES #### 
+library(here)
+# library(infer)
+suppressPackageStartupMessages(library(tidyverse))
+library(sdmTMB) 
+source(here("R", "StratMeanFXs_v2.R"))
+source(here("sdmtmb", "R", "sim_prep_functions.R"))
+
+
+### ENVIRONMENT SET UP ####
+set.seed(72346)
+# set season and species to be simulated
+season <- "fall"
+
+species <- "sumflounder"
+
+# number of simulations 
+nsims <- 1:1000
+
+# set file locations of where to read in data and save outputs
+#dat.file <- here("data", species)
+dat.files <- here("sdmtmb", species, "data")
+sim.dat <- here("sdmtmb", species, "data", "simulations")#, "min-sims")
+plot.files <- here("sdmtmb", species, "plots", "simulations")
+
+# generate a set random numbers to set seed with each iteration for reproducibility
+seed <- sample.int(1e6, length(nsims))
+
+
+### LOAD DATA ####
+# fitted sdmTMB model 
+mod <- readRDS(here(dat.files, str_c(season, "_mod.rds", sep = "")))
+
+# depth relationship coefficients used in model fitting
+depth_coefs <- readRDS(here(dat.files, str_c(season, "_depth_coefs.rds", sep = "")))
+
+
+# model predictions 
+mod_preds <- readRDS(here(dat.files, str_c(season, "_mod_preds.rds", sep = ""))) |> 
+  select(X, Y, YEAR, SEASON, TOWID, STATION, CRUISE6, STRATUM, AVGDEPTH, AREA, AREA_CODE,  omega_s) |>  # select important tow information
+  mutate(AREA = as.character(AREA), # transform factor into character
+         AREA_CODE = ifelse(AREA == "OUTSIDE", 0, AREA_CODE)) |> # transform area_code to match the simulation output field 
+  rename(fix_omegas = omega_s, # rename spatial predictions 
+         EST_YEAR = YEAR) # rename year to match formula and model fit
+
+# grid predictions 
+grid_preds <- readRDS(here(dat.files, str_c(season,"_grid_preds.rds", sep = ""))) |> 
+  dplyr::select(!c(est, est_non_rf, est_rf, epsilon_st)) |> 
+  # convert factors back to integers and characters
+  mutate(EST_YEAR = as.integer(as.character(EST_YEAR)), 
+         AREA = as.character(AREA), 
+         AREA_CODE = ifelse(AREA == "OUTSIDE", 0, AREA_CODE)) |> 
+  rename(fix_omegas = omega_s)
+
+# load the active bottom trawl survey strata and their relative area weights created here(tidy-data, "02b-filter-current-strata.R")
+strata <- readRDS(here("data", "rds", "active_strata_wts.rds"))
+
+
+#### DATA WRANGLE #### 
+# extract the number of depth coefficients 
+n_depth_coefs <- length(depth_coefs)
+
+# identify the unique years to sample 
+years <- unique(mod_preds$EST_YEAR) |> sort()
+
+# extract fitted model coefficients for data simulation
+mod_coefs <- tidy(mod) |> 
+  dplyr::select(!std.error)
+
+# extract the random effect parameters for data simulation
+ran_pars <- tidy(mod, "ran_pars")
+
+# save the data 
+saveRDS(ran_pars, here(dat.files, str_c(season,  "mod_ran_pars.rds", sep = "_")))
+
+
+## DATA PREPARATION ####
+# number of years to project
+nyears <- 1:5
+
+# sample the unique years from the survey for the size of nyears, dictate the number of iterations to sample years, and set the seed with each iteration to allow for reproducibility. 
+year_samps <- map2(nsims, seed, ~sample_years(nyears, years, seed = .y))
+
+# save the data
+saveRDS(year_samps, here(sim.dat, str_c(season, species, length(nsims), "year_samps.rds", sep = "_")))
+
+# create a list item containing the historical survey locations replicated by the number of nsims 
+sim_locs <- map(nsims, ~list(data = mod_preds))
+
+# save the data
+saveRDS(sim_locs, here(sim.dat, str_c(season, species, length(nsims), "surv_locs.rds", sep = "_")))
+
+# create a list item containing the NE US grid replicated by the number of nsims 
+sim_grid <- map(nsims, ~list(data = grid_preds))
+# future_locs <- map2(year_samps, seed, ~survey_locations(.x, mod_preds, seed = .y))
+
+# save the data
+saveRDS(sim_grid, here(sim.dat, str_c(season, species, length(nsims), "surv_grid.rds", sep = "_")))
+
+# create mesh for survey locations 
+mesh <- make_mesh(mod_preds, xy_cols = c("X", "Y"), cutoff = 10) 
+# create a list item containing the survey location mesh replicated by the number of nsims
+surv_meshs <- map(nsims, ~list(mesh = mesh))
+# surv_meshs <- map(surv_locs, ~make_mesh(.x, xy_cols = c("X", "Y"), cutoff = 10)) |> map(~list(mesh = .))
+# surv_meshs <- map(sim_locs, ~make_mesh(.$data, xy_cols = c("X", "Y"), cutoff = 10)) |> map(~list(mesh = .))
+
+# save the data
+saveRDS(surv_meshs, here(sim.dat, str_c(season, species, length(nsims), "surv_meshs.rds", sep = "_")))
+
+# create mesh for the grid locations 
+mesh <- make_mesh(grid_preds, xy_cols = c("X", "Y"), cutoff = 10) 
+# create a list item containing the grid mesh replicated by the number of nsims
+grid_meshs <- map(nsims, ~list(mesh = mesh))
+
+# save the data
+saveRDS(grid_meshs, here(sim.dat, str_c(season, species, length(nsims), "grid_meshs.rds", sep = "_")))
+
+### BASE SCENARIO ####
+# extract the parameter coefficients 
+coefs <- map(nsims, ~prepare_coefs(mod_coefs, n_depth_coefs, wind_coef = TRUE, treatment = 0)) |> 
+  map(~list(b = .))
+
+# create a list object where each item contains the survey locations, the mesh, and the parameter coefficients  
+surv_data <- map2(sim_locs, surv_meshs, ~append(.x, .y)) |> 
+  map2(coefs, ~append(.x, .y))
+
+# create a list object where each item contains the grid locations, the mesh, and the parameter coefficients
+grid_data <- map2(sim_grid, grid_meshs, ~append(.x, .y)) |> 
+  map2(coefs, ~append(.x, .y))
+
+### save the data 
+saveRDS(coefs, here(sim.dat, str_c(season, species, "baseline", length(nsims), "coefs.rds", sep = "_")))
+saveRDS(surv_data, here(sim.dat, str_c(season, species, "baseline", length(nsims), "surv-data.rds", sep = "_")))
+saveRDS(grid_data, here(sim.dat, str_c(season, species, "baseline", length(nsims), "grid-data.rds", sep = "_")))
+
+
+
+### ENHANCED SCENARIO ####
+# extract the parameter coefficients 
+coefs <- map(nsims, ~prepare_coefs(mod_coefs, n_depth_coefs, wind_coef = TRUE, treatment = (log(2)))) |> 
+  map(~list(b = .))
+
+# create a list object where each item contains the survey locations, the mesh, and the parameter coefficients  
+surv_data <- map2(sim_locs, surv_meshs, ~append(.x, .y)) |> 
+  map2(coefs, ~append(.x, .y))
+
+# create a list object where each item contains the grid locations, the mesh, and the parameter coefficients
+grid_data <- map2(sim_grid, grid_meshs, ~append(.x, .y)) |> 
+  map2(coefs, ~append(.x, .y))
+
+### save the data 
+saveRDS(coefs, here(sim.dat, str_c(season, species, "enhanced", length(nsims), "coefs.rds", sep = "_")))
+saveRDS(surv_data, here(sim.dat, str_c(season, species, "enhanced", length(nsims), "surv-data.rds", sep = "_")))
+saveRDS(grid_data, here(sim.dat, str_c(season, species, "enhanced", length(nsims), "grid-data.rds", sep = "_")))
+
+
+### REDUCED SCENARIO ####
+# extract the parameter coefficients 
+coefs <- map(nsims, ~prepare_coefs(mod_coefs, n_depth_coefs, wind_coef = TRUE, treatment = (-log(2)))) |> 
+  map(~list(b = .))
+
+# create a list object where each item contains the survey locations, the mesh, and the parameter coefficients  
+surv_data <- map2(sim_locs, surv_meshs, ~append(.x, .y)) |> 
+  map2(coefs, ~append(.x, .y))
+
+# create a list object where each item contains the grid locations, the mesh, and the parameter coefficients
+grid_data <- map2(sim_grid, grid_meshs, ~append(.x, .y)) |> 
+  map2(coefs, ~append(.x, .y))
+
+### save the data 
+saveRDS(coefs, here(sim.dat, str_c(season, species, "reduced", length(nsims), "coefs.rds", sep = "_")))
+saveRDS(surv_data, here(sim.dat, str_c(season, species, "reduced", length(nsims), "surv-data.rds", sep = "_")))
+saveRDS(grid_data, here(sim.dat, str_c(season, species, "reduced", length(nsims), "grid-data.rds", sep = "_")))
