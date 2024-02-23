e17eb9350884459073be167dd2f6a20eeda71861
Lia Miller <amiller7@umassd.edu>
2024-02-22 22:08:17 -0500

    add functions to calculate error statistics

--- R/StratMeanFXs_v2.R
+++ R/StratMeanFXs_v2.R
@@ -1,12 +1,23 @@
 #' Calculate estimates of stratified mean abundance 
 #'
-#' Calculate the random stratified mean catch rate to serve as an index of relative abundance according to a random stratified survey design (Krebs, Ch.8, Eq. 8.17)
+#' @description Calculate the random stratified mean catch rate to serve as an index of relative abundance according to a random stratified survey design (Krebs, Ch.8, Eq. 8.17)
 #'
 #' @param dat: survey catch rate data
 #' @param strata: dataframe of survey strata footprint containing per strata area information
 #'
 #' @returns
 #' A dataframe containing annual estimates of abundance and survey coefficients of variation for a given species similar to those used as inputs for species stock assessments.
+#'
+#'
+#'@examples
+#'
+#' strata <- readRDS(here("data","rds", "active_strata_wts.rds"))
+#' dat <- data.frame(STRATUM = c(1650, 1650, 1650, 1650, 3230, 3230, 3230, 1010, 1010, 1010, 1010, 3050, 3050, 3050), 
+#'                   STATION = c(56, 18, 45, 225, 4, 5, 50, 65, 180, 303, 314, 27, 8, 178), 
+#'                   EXPCATCHWT = c(10, 5, 10, 5, 1,1,1, 10, 5, 10, 5, 1, 1, 1))
+#'stratified.mean(dat, strata) 
+#'
+#'
 #'
 
 
@@ -16,7 +27,7 @@ stratified.mean <- function(dat, strata){
   strat_use <- unique(dat$STRATUM)
   
   #adjust the weights for the strata used in the calculation
-  strata_wts_use <- strata_wts |>
+  strata_wts_use <- strata |>
     filter(STRATUM %in% strat_use) |>
     mutate(RelWt = Area_SqNm / sum(Area_SqNm))
   
@@ -48,27 +59,72 @@ stratified.mean <- function(dat, strata){
   #return(individual)
 }  
 
-#' Calculate mean relative percent difference  
+#' Calculate error
 #'
-#' Calculate the mean relative percent difference between two estimates of random stratified mean abundance indices under survey effort treatments 
+#' @description calculates bias, relative bias, and absolute relative bias
 #'
 #' @param dat: dataframe containing differing values of random stratified abundance indices output from `stratified.mean()`
+#' @param observed: column name containing observed values 
+#' @param expected: column name containing expected values 
+#' 
 #'
-#' @returns
-#' A dataframe containing the mean relative percent difference between survey effort stratified abundance indices.
+#' @returns A dataframe where:
+#' * `error` represents the bias or error 
+#' * `rel.err` represents the relative bias 
+#' * `abs.err` represents the absolute relative error 
 #'
+#' @examples
+#' dat <- data.frame(EST_YEAR = c(2009, 2010, 2015), 
+#'                   stratmu_x = c(5, 10, 2), 
+#'                   stratmu_y = c(2.5, 5, 1))
+#' calc.errors(dat, stratmu_x, stratmu_y)
+#' 
+#' 
 
-mean.diff <- function(dat){
+calc.errors <- function(dat, observed, expected){
 
-mudiff <- dat |> 
-  group_by(EST_YEAR) |> 
-  summarise(diff_mu = diff(stratmu), .groups = "drop") |>
-  mutate(exp_mu = (exp(diff_mu))-1,
-         sq_diff = exp_mu^2) |>
-  filter(EST_YEAR %in% tail(EST_YEAR, 5)) |> 
-  summarize(mudiff = mean(sq_diff), .groups = "drop") |> # calculate the average; drop the grouping factor 
-  mutate(mudiff = sqrt(mudiff)*100) 
-
-return(mudiff)
+error.dat <- dat |> 
+  mutate(error = {{observed}} - {{expected}},
+         rel.err = error / {{observed}}, 
+         abs.err = abs(rel.err)) |> 
+  mutate(error = ifelse(is.nan(error), 0, error), 
+         rel.err = ifelse(is.nan(rel.err), 0, rel.err), 
+         abs.err =  ifelse(is.nan(abs.err), 0, abs.err))
+  
+return(error.dat)
   
 }
+
+#' Calculate mean percent differences 
+#'
+#' @description Calculate the mean percent difference 
+#'
+#' @param errors: dataframe containing differing values of random stratified abundance indices output from `calc.errors()`
+#' 
+#'
+#' @returns A dataframe where:
+#' * `ME` represents the mean error
+#' * `MRE_perc` represents the mean percent relative difference
+#' * `MARE_perc` represents the mean percent absolute relative difference 
+#'
+#' @examples
+#' dat <- data.frame(EST_YEAR = c(2009, 2010, 2015), 
+#'                   stratmu_x = c(5, 10, 2), 
+#'                   stratmu_y = c(2.5, 5, 1))
+#' errors <- calc.errors(dat, stratmu_x, stratmu_y)
+#' mean.diff(errors)
+#'                   
+#' 
+
+mean.diff <- function(error.dat){
+  
+  mudiff <- error.dat |> 
+    summarise(ME = mean(error), 
+              MRE_perc = mean(rel.err)*100, 
+              MARE_perc = mean(abs.err)*100)
+  
+  return(mudiff)
+  
+}
+
+                  
