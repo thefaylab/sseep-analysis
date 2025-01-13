6b868e155f8cbf339a5564996e532b8fed2f9ac2
Lia Miller <amiller7@umassd.edu>
2025-01-12 20:22:12 -0500

    added plots for a survey biomass map and number of tows over time

--- sumflounder/04-filter-summer-flounder.R
+++ sumflounder/04-filter-summer-flounder.R
@@ -1,5 +1,5 @@
 ### created: 07/24/2023
-### last updated: 12/05/2023
+### last updated: 11/11/2024
 
 # 04 - FILTER SUMMER FLOUNDER DATA ####
 
@@ -10,11 +10,11 @@
 
 ### LOAD PACKAGES ####
 # library(stringr)
-# library(sf)
-# library(patchwork)
+library(sf)
+library(patchwork)
 library(here)
 suppressPackageStartupMessages(library(tidyverse))
-
+theme_set(theme_bw())
 
 ### LOAD AND FILTER DATA ####
 
@@ -33,6 +33,12 @@ fall_strata <- unique(fall_footprint$STRATUM)
 spring_footprint <- readRDS(here("data", "sumflounder", "sf_spring_footprint.rds"))
 spring_strata <- unique(spring_footprint$STRATUM)
 
+# read in the northeast coastline
+ne_coast <- sf::st_read(dsn = here("gis", "eastern_coast_WGS84.shp"))
+
+# active strata shapefile 
+strata <- readRDS(here("data", "rds", "active_strata.rds"))
+
 
 ## FILTER DATA ####
 # fall data 
@@ -46,6 +52,41 @@ sf_spring <- sumflounder_complete |>
 # full dataset 
 sumflounder <- bind_rows(sf_spring, sf_fall)
 
+## PLOT THE OBSERVATIONS ####
+obs_plot <- ggplot() +
+  geom_sf(data = ne_coast, fill = "#efe5c7", color = "#816c62") +
+  geom_sf(data = strata, fill = NA) +
+  geom_point(data = sumflounder, aes(x = DECDEG_BEGLON, y = DECDEG_BEGLAT, color = EXPCATCHWT, size = EXPCATCHWT, alpha = EXPCATCHWT)) +
+  scale_colour_viridis_c(guide = "legend")+
+  scale_size(range=c(1,10)) +
+  scale_alpha_continuous(range = c(0.4, 1)) +
+  labs(x = "Longitude", y = "Latitude", color = "Biomass\n(kg/tow)", size = "Biomass\n(kg/tow)", alpha = "Biomass\n(kg/tow)") +
+  facet_grid(rows = vars(str_to_title(AREA)), cols = vars(str_to_title(SEASON))) +
+  theme(panel.background = element_blank(), panel.grid = element_blank())
+
+## PLOT WIND OVER TIME ####
+sumflounder_summ <- sumflounder |> 
+  group_by(EST_YEAR, AREA, SEASON) |> 
+  summarise(sum_tow = length(unique(TOWID)), 
+            sum_catch = sum(EXPCATCHNUM), 
+            sum_bio = sum(EXPCATCHWT))
+
+# sumflounder_summ |> group_by(AREA, SEASON) |> summarise(sum(sum_tow))
+# sumflounder_summ |> group_by(AREA, SEASON) |> summarise(mean(sum_tow), sd(sum_tow))
+
+wind_tows_plot <- ggplot(sumflounder_summ |> filter(AREA == "WIND")) +
+  # geom_col(aes(x = as.factor(EST_YEAR), y = sum_tow, fill = str_to_title(AREA)), position = "fill") +
+  geom_line(aes(x = EST_YEAR, y = sum_tow, color = str_to_title(AREA))) +
+  labs(x = "Year", y = "Number of survey tows", color = "Area") +
+  # geom_bar(aes(x = as.factor(EST_YEAR), y = sum_tow, fill = AREA), stat_count(sum_tow), position = "fill") +
+  facet_wrap(~str_to_title(SEASON)) +
+  ylim(0, NA)
+
+all_tows_plot <- ggplot(sumflounder_summ) +
+  geom_line(aes(x = EST_YEAR, y = sum_tow, color = str_to_title(AREA))) +
+  labs(x = "Year", y = "Number of survey tows", color = "Area") +
+  facet_wrap(~str_to_title(SEASON)) +
+  ylim(0, NA)
 
 # stratified mean abundance indices created here("retro-analysis") 
 # sf_stratmu <- readRDS(here("data", "rds", "retro-analysis", "strat-mu_all.rds")) |> 
@@ -61,10 +102,22 @@ sumflounder <- bind_rows(sf_spring, sf_fall)
 # sf_slopes <- readRDS(here("retro-analysis", "active_strata_wts_only", "obs-stratmu-slopes.rds")) |>
 #   filter(SVSPP == 103)
 
+
+
 ### save the data 
-saveRDS(sumflounder, here("data", "sumflounder", "sumflounder.rds"))
-saveRDS(sf_fall, here("data", "sumflounder", "sumflounder_fall.rds"))
-saveRDS(sf_spring, here("data", "sumflounder", "sumflounder_spring.rds"))
+data <- list("sumflounder" = sumflounder, 
+             "sumflounder_fall" = sf_fall, 
+             "sumflounder_spring" = sf_spring)
+plots <- list("biomass_plot" = obs_plot, 
+              "wind_tows_plot" = wind_tows_plot, 
+              "all_tows_plot" = all_tows_plot)
+
+pmap(list(data, names(data)), ~saveRDS(.x, file = str_c(.y, "rds", sep = ".",), path = here("data", "sumflounder")))
+pmap(list(plots, names(plots)), ~ggsave(plot = .x, filename = str_c(.y, "png", sep = "."), device = "png", path = here("outputs", "init-analysis-plots", "sumflounder"), width = 12, height = 8))
+  
+# saveRDS(sumflounder, here("data", "sumflounder", "sumflounder.rds"))
+# saveRDS(sf_fall, here("data", "sumflounder", "sumflounder_fall.rds"))
+# saveRDS(sf_spring, here("data", "sumflounder", "sumflounder_spring.rds"))
 # saveRDS(sf_stratmu, here("data", "sumflounder", "sf_stratmu.rds"))
 # saveRDS(sf_mudiff, here("data", "sumflounder", "sf_mudiff.rds"))
 # saveRDS(sf_slopes, here("data", "sumflounder", "sf_obs-slopes.rds"))
